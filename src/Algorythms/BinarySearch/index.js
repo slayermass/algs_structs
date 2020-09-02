719b221bfb061bca8a7009d6d69c8bd09416d74d
Артём Нехорошков <a.nekhoroshkov@freematiq.com>
2020-09-02 10:14:32 +0700

    add binarySearchLoop

--- src/Algorythms/BinarySearch/index.js
+++ src/Algorythms/BinarySearch/index.js
@@ -2,20 +2,42 @@
  * двоичный(бинарный) поиск
  * arr должен быть отсортирован
  */
-const binarySearch = (arr, val) => {
+
+/** рекурсивный поиск */
+const binarySearchRecursive = (arr, val) => {
   const middle = Math.floor(arr.length / 2);
   const middleVal = arr[middle];
-
-  // console.log(middleVal, val);
 
   if (middleVal === val) {
     return middleVal;
   } else if (val < middleVal && arr.length > 1) {
-    return binarySearch(arr.splice(0, middle), val);
+    return binarySearchRecursive(arr.splice(0, middle), val);
   } else if (val > middleVal && arr.length > 1) {
-    return binarySearch(arr.splice(middle, Number.MAX_VALUE), val);
+    return binarySearchRecursive(arr.splice(middle, Number.MAX_VALUE), val);
   }
   return 'not found';
 };
 
-module.exports = binarySearch;
+/** поиск с циклом while, изменение условия на каждой итерации */
+const binarySearchLoop = (arr, val) => {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start <= end) {
+    const middle = Math.floor((start + end) / 2);
+    const middleVal = arr[middle];
+
+    if (middleVal === val) {
+      return middleVal;
+    }
+
+    if (val < middleVal) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+  }
+  return 'not found';
+}
+
+module.exports = { binarySearchRecursive, binarySearchLoop };
--- src/Algorythms/BinarySearch/index.test.js
+++ src/Algorythms/BinarySearch/index.test.js
@@ -1,20 +1,40 @@
-const binarySearch = require('./index');
+const { binarySearchRecursive, binarySearchLoop } = require('./index');
 
-describe('--- binarySearch ---', () => {
+describe('--- binarySearchRecursive ---', () => {
   test('found values', () => {
-    expect(binarySearch([1,2,3,4,5], 4)).toEqual(4);
-    expect(binarySearch([1,2,3,4,5,6], 1)).toEqual(1);
-    expect(binarySearch([1], 1)).toEqual(1);
+    expect(binarySearchRecursive([1,2,3,4,5], 4)).toEqual(4);
+    expect(binarySearchRecursive([1,2,3,4,5,6], 1)).toEqual(1);
+    expect(binarySearchRecursive([1], 1)).toEqual(1);
   });
 
   test('no found values', () => {
-    expect(binarySearch([1,2,3,4,5], 50)).toEqual('not found');
-    expect(binarySearch([1,2,3,4,5,6], 11)).toEqual('not found');
-    expect(binarySearch([1], -1)).toEqual('not found');
+    expect(binarySearchRecursive([1,2,3,4,5], 50)).toEqual('not found');
+    expect(binarySearchRecursive([1,2,3,4,5,6], 11)).toEqual('not found');
+    expect(binarySearchRecursive([1], -1)).toEqual('not found');
   });
 
   test('negative values', () => {
-    expect(binarySearch([-5,-4,-3,-2,-1], 50)).toEqual('not found');
-    expect(binarySearch([-6, -5,-4,-3,-2,-1], 11)).toEqual('not found');
+    expect(binarySearchRecursive([-5,-4,-3,-2,-1], 50)).toEqual('not found');
+    expect(binarySearchRecursive([-6, -5,-4,-3,-2,-1], 11)).toEqual('not found');
+  });
+});
+
+
+describe('--- binarySearchLoop ---', () => {
+  test('found values', () => {
+    expect(binarySearchLoop([1,2,3,4,5], 4)).toEqual(4);
+    expect(binarySearchLoop([1,2,3,4,5,6], 1)).toEqual(1);
+    expect(binarySearchLoop([1], 1)).toEqual(1);
+  });
+
+  test('no found values', () => {
+    expect(binarySearchLoop([1,2,3,4,5], 50)).toEqual('not found');
+    expect(binarySearchLoop([1,2,3,4,5,6], 11)).toEqual('not found');
+    expect(binarySearchLoop([1], -1)).toEqual('not found');
+  });
+
+  test('negative values', () => {
+    expect(binarySearchLoop([-5,-4,-3,-2,-1], 50)).toEqual('not found');
+    expect(binarySearchLoop([-6, -5,-4,-3,-2,-1], 11)).toEqual('not found');
   });
 });
