7e108fd20881c22769c1187b60e6fef8b8994f04
Artem Nekhoroshkov <anekhoroshkov@aexsoft.com>
2022-09-16 17:22:29 +0700

    arrayQuickSort improvement

--- src/Algorithms/Array/QuickSort/index.test.js
+++ src/Algorithms/Array/QuickSort/index.test.js
@@ -1,20 +1,17 @@
 const arrayQuickSort = require('./index');
 const {getArrayFilledRandomIntegers, sortIntegerArray} = require('../../../utils/miscellaneous');
 
-
 describe('--- arrayQuickSort ---', () => {
   test('base', () => {
     expect(arrayQuickSort([20, 500, 3, 80, 66, 42])).toEqual([3, 20, 42, 66, 80, 500]);
-    expect(arrayQuickSort([
-      1298, 2, 2677, 2681, 3600, 3796, 42, 4430, 714, 819
-    ])).toEqual([
-      2, 42, 714, 819, 1298, 2677, 2681, 3600, 3796, 4430
-    ]);
+    expect(arrayQuickSort([-20, -8980980, 3, 80, 6600003, 0])).toEqual([-8980980, -20, 0, 3, 80, 6600003]);
+    expect(arrayQuickSort([1298, 2, 2677, 2681, 3600, 3796, 42, 4430, 714, 819])).toEqual([2, 42, 714, 819, 1298, 2677, 2681, 3600, 3796, 4430]);
   });
 
   test('with duplicates', () => {
     expect(arrayQuickSort([20, 20, 500, 3, 80, 66, 42])).toEqual([3, 20, 20, 42, 66, 80, 500]);
     expect(arrayQuickSort([16, 16, 160, 160, 55, 14, 898])).toEqual([14, 16, 16, 55, 160, 160, 898]);
+    expect(arrayQuickSort([1, 1, 1, 2, 2, 2, 5, 5, 5, 5, 5, 6, 7, 7, 7, 7, 7, 7])).toEqual([1, 1, 1, 2, 2, 2, 5, 5, 5, 5, 5, 6, 7, 7, 7, 7, 7, 7]);
   });
 
   test('base generate', () => {
